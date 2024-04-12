3dd957f7b72e0d584d95ff06f604ff6a67e2ed87
casualdevjl <devjlumba@gmail.com>
2024-04-13 06:00:15 +0800

    Cleaned script, local storage save added

--- project-2-movie-seat-booking/js/script.js
+++ project-2-movie-seat-booking/js/script.js
@@ -1,51 +1,113 @@
 const movieSelect = document.getElementById('movie');
-const seats = document.querySelectorAll('.row .seat');
 
-movieSelect.addEventListener('change', updateSelectedMovie);
-
-seats.forEach(seat => seat.addEventListener('click', toggleSelection));
+const seats = document.querySelectorAll('.seat');
 
 let ticketPrice = +movieSelect.value;
 
-function toggleSelection(e) {
-    const seat = e.target; // Targetted element assigned as 'seat'
-    console.log(e.target);
+populateUI();
 
+// Event listener for movie selection
+movieSelect.addEventListener('change', (e) => {
+    // Reset previous selection
+    resetSelectedSeats();
+
+    // Get selected movie data from array of objects
+    ticketPrice = +e.target.value;
+
+    // Set selection index and price
+    const selectedMovieIndex = e.target.selectedIndex;
+    
+    const selectedMoviePrice = e.target.value;
+
+    // Save selection to local storage
+    localStorage.setItem('selectedMovieIndex', selectedMovieIndex);
+    localStorage.setItem('selectedMoviePrice', selectedMoviePrice);
+
+    updateTotalCost();
+})
+
+// Event listener for seat selection
+seats.forEach(seat => seat.addEventListener('click', toggleSeatSelection))
+
+// Toggle seat selection
+function toggleSeatSelection(e) {
+    const seat = e.target;
+    console.log(seat);
+
+    // Check if selected seat is not occupied
     if (!seat.classList.contains('occupied')) {
-        console.log("Seat is not occupied. Toggling selected class.");
+        console.log('Seat is not occupied. Toggling "selected" class.')
+
         seat.classList.toggle('selected');
-        console.log(seat.classList)
+
+        // Check if 'selected' class is added to the element
+        console.log(seat.classList);
+
+        // Save selection to local storage
+        saveSelectedSeats();
     } else {
-        console.log("Seat is occupied. Cannot select.");
+        console.log('Seat is occupied, cannot select.');
     }
 
     updateTotalCost();
 }
 
-function updateSelectedMovie(e) {
-    ticketPrice = +e.target.value;
-
-    // Reset seats when new movie is selected
-    resetSelectedSeats();
-    updateTotalCost();
-}
-
+// Update total cost
 function updateTotalCost() {
-    const selectedSeats = document.querySelectorAll('.seat.selected'); // Selects elements with class 'seat selected'
+    // Select all elements with class "seat selected"
+    const selectedSeats = document.querySelectorAll('.seat.selected');
 
-    const numberOfSeats = selectedSeats.length; // Selects number of elements with class 'seat selected'
+    const numberOfSeats = selectedSeats.length;
 
     const totalCost = numberOfSeats * ticketPrice;
 
-    // Display info to the user
+    // Display info to user
     document.getElementById('totalPrice').textContent = `${totalCost}`;
+
+    document.getElementById('ticketCount').textContent = `${numberOfSeats}`;
 }
 
+// Save selected seats to local storage
+function saveSelectedSeats() {
+    const selectedSeats = document.querySelectorAll('.seat.selected');
+
+    // Get index position of each selected seats in an array
+    const seatsIndex = [...selectedSeats].map((seat => [...seats].indexOf(seat)));
+
+    // Save to local storage
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+}
+
+// Reset selected seats when movie selection changes
 function resetSelectedSeats() {
-    // Reset selection when new movie is selected
     const selectedSeats = document.querySelectorAll('.seat.selected');
 
     selectedSeats.forEach(seat => {
         seat.classList.remove('selected');
     });
+}
+
+// Populate UI with saved data
+function populateUI() {
+    // Get stored movie index data from local storage
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    // Get stored selected seats data from local storage
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    // Check if there's a selected movie data stored
+    if (selectedMovieIndex !== null) {
+        // Show the selected movie in the UI
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+
+    // Check if there are selected seats stored
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        // Display seats
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        })
+    }
 }
